45c2e729f81310a20c9a5abcae007725f17d0754
Haiyue Wang <haiyue.wang@intel.com>
2022-11-12 20:42:39 +0800

    Implement to configure the Tokio worker threads number
    Builds Tokio Runtime with custom configuration values, instead of fixed
    setting by macro '#[tokio::main]'.
    
    Since the ztunnel works on node level, the workloads will be heavy, add
    one environment variable for tuning the Tokio worker threads number.
    
    Use 'istioctl --set values.ztunnel.env.ZTUNNEL_WOKRER_THREADS=5' to set
    worker threads if needed, and default value is 2.
    
    Signed-off-by: Haiyue Wang <haiyue.wang@intel.com>

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,18 @@ use ztunnel::*;
 // #[global_allocator]
 // static GLOBAL: tcmalloc::TCMalloc = tcmalloc::TCMalloc;
 
-#[tokio::main(worker_threads = 2)]
-async fn main() -> anyhow::Result<()> {
+fn main() {
     telemetry::setup_logging();
     let config = ztunnel::config::Config {
         ..Default::default()
     };
-    app::spawn(signal::Shutdown::new(), config).await
+
+    tokio::runtime::Builder::new_multi_thread()
+        .worker_threads(config.num_worker_threads)
+        .enable_all()
+        .build()
+        .unwrap()
+        .block_on(async move {
+            let _ = app::spawn(signal::Shutdown::new(), config).await;
+        });
 }
