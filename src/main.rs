7d8dc28c0370404aeb5d883ebc25857e8cc4e2fa
Haiyue Wang <haiyue.wang@intel.com>
2022-11-16 00:25:33 +0800

    Implement to configure the Tokio worker threads number (#112)
    * Implement to configure the Tokio worker threads number
    
    Builds Tokio Runtime with custom configuration values, instead of fixed
    setting by macro '#[tokio::main]'.
    
    Since the ztunnel works on node level, the workloads will be heavy, add
    one environment variable for tuning the Tokio worker threads number.
    
    Use 'istioctl --set values.ztunnel.env.ZTUNNEL_WOKRER_THREADS=5' to set
    worker threads if needed, and default value is 2.
    
    Signed-off-by: Haiyue Wang <haiyue.wang@intel.com>
    
    * Keep the errors handling for new main function
    
    We can use the command 'cargo +nightly rustc -- -Z unpretty=hir' to dump
    how '#[tokio::main]' expands the main function with errors handling:
    
      #[tokio::main(worker_threads = 2)]
      async fn main() -> Result<(), Box<dyn std::error::Error>> {
    
    will be expanded to:
    
      fn main() -> Result<(), Box<dyn std::error::Error>> {
          let
              body =
              #[lang = "from_generator"](
    
          tokio::runtime::Builder::new_multi_thread()
                .worker_threads(2usize).enable_all().build()
                .expect("Failed building the Runtime").block_on(body);
      }
    
    So just keep the errors returned from 'app::spawn' by new main function.
    
    Signed-off-by: Haiyue Wang <haiyue.wang@intel.com>
    
    Signed-off-by: Haiyue Wang <haiyue.wang@intel.com>

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,16 @@ use ztunnel::*;
 // #[global_allocator]
 // static GLOBAL: tcmalloc::TCMalloc = tcmalloc::TCMalloc;
 
-#[tokio::main(worker_threads = 2)]
-async fn main() -> anyhow::Result<()> {
+fn main() -> anyhow::Result<()> {
     telemetry::setup_logging();
     let config = ztunnel::config::Config {
         ..Default::default()
     };
-    app::spawn(signal::Shutdown::new(), config).await
+
+    tokio::runtime::Builder::new_multi_thread()
+        .worker_threads(config.num_worker_threads)
+        .enable_all()
+        .build()
+        .unwrap()
+        .block_on(async move { app::spawn(signal::Shutdown::new(), config).await })
 }
