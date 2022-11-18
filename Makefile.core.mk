5963b1417135b7a3a007347a21279af1c06a4bda
Loong Dai <loong.dai@intel.com>
2022-11-18 08:21:03 +0800

    chore: fix lint issues (#142)
    * update common-files
    
    Signed-off-by: Loong <loong.dai@intel.com>
    
    * add copyright banner
    
    Signed-off-by: Loong <loong.dai@intel.com>
    
    * override target
    
    Signed-off-by: Loong <loong.dai@intel.com>
    
    * add comment
    
    Signed-off-by: Loong <loong.dai@intel.com>
    
    * update fix target
    
    Signed-off-by: Loong <loong.dai@intel.com>
    
    * revert Makefile.overrides.mk
    
    Signed-off-by: Loong <loong.dai@intel.com>
    
    * fix CI
    
    Signed-off-by: Loong <loong.dai@intel.com>
    
    Signed-off-by: Loong <loong.dai@intel.com>

--- Makefile.core.mk
+++ Makefile.core.mk
@@ -6,13 +6,20 @@ test:
 build:
 	cargo build
 
+# override target in common/Makefile.common.mk, only check golang and rust codes
+# load order: Makefile -> Makefile.overrides -> Makefile.core -> Makefile.common
+# cannot override in Makefile.overrides
+lint-copyright-banner:
+	@${FINDFILES} \( -name '*.go' -o -name '*.rs' \) \( ! \( -name '*.gen.go' -o -name '*.pb.go' -o -name '*_pb2.py' \) \) -print0 |\
+		${XARGS} common/scripts/lint_copyright_banner.sh
+
 lint: lint-scripts lint-yaml lint-markdown lint-licenses lint-copyright-banner
 	cargo clippy
 
 check:
 	cargo check
 
-fix:
+fix: fix-copyright-banner
 	cargo clippy --fix --allow-staged --allow-dirty
 	cargo fmt
 
