d3be371f7865e6aa9e06819b7df51c1788dce552
John Howard <howardjohn@google.com>
2023-01-11 13:15:02 -0800

    debug logs

--- Makefile.core.mk
+++ Makefile.core.mk
@@ -1,7 +1,7 @@
 include common/Makefile.common.mk
 
 test:
-	cargo test --benches --tests --bins
+	RUST_LOG=debug cargo test --benches --tests --bins
 
 test-root: export CARGO_TARGET_$(shell rustc -vV | sed -n 's|host: ||p' | tr [:lower:] [:upper:]| tr - _)_RUNNER = sudo -E
 test-root:
