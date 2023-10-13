a0d96fecb2e8beb5538ecfc6a7d11247ca12074b
John Howard <howardjohn@google.com>
2023-10-12 22:02:34 -0500

    Drop experimental protoc flag (#695)

--- build.rs
+++ build.rs
@@ -33,7 +33,6 @@ fn main() -> Result<(), anyhow::Error> {
     let config = {
         let mut c = prost_build::Config::new();
         c.disable_comments(Some("."));
-        c.protoc_arg("--experimental_allow_proto3_optional");
         c.bytes([
             ".istio.workload.Workload",
             ".istio.workload.Service",
