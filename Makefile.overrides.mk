4a7f2fc609c4f06fb48bca476d288fcf088bf029
Benjamin Leggett <benjamin.leggett@solo.io>
2024-02-28 17:45:50 -0500

    Hint: cannot run tests as root if container is not running as privileged
    Signed-off-by: Benjamin Leggett <benjamin.leggett@solo.io>

--- Makefile.overrides.mk
+++ Makefile.overrides.mk
@@ -1,2 +1,15 @@
 # Use the build container by default
 BUILD_WITH_CONTAINER ?= 1
+
+# Cannot run root tests in container unless the container is privileged,
+# and we cannot/should not run it as privileged by default - so communicate that.
+ifeq ($(TEST_MODE), root)
+    ifeq ($(BUILD_WITH_CONTAINER), 1)
+        $(info ***** NOTE ******)
+        $(info TEST_MODE=root and BUILD_WITH_CONTAINER=1)
+        $(info If you wish to run tests that require root privilege inside the build container,)
+        $(info you must run the container as privileged yourself.)
+        $(info Alternatively, set BUILD_WITH_CONTAINER=0 and run the tests as root outside the build container.)
+        $(info ***** NOTE ******)
+	endif
+endif
