fcbf3532453934fd7490ea93aa8d2d4e3d8a042f
John Howard <howardjohn@google.com>
2024-04-11 11:24:30 -0700

    version: include profile name (#916)
    This will help debug "My ztunnel is slow!!" issues when running in debug
    mode.

--- build.rs
+++ build.rs
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use std::env;
 use std::process::Command;
 
 // This build script is used to generate the rust source files that
@@ -63,6 +64,13 @@ fn main() -> Result<(), anyhow::Error> {
         println!("cargo:rerun-if-changed={}", path.to_str().unwrap());
     }
 
+    // Adoppted from https://github.com/uutils/coreutils/blob/main/src/uu/stdbuf/build.rs
+    let out_dir = env::var("OUT_DIR").unwrap();
+    let profile_name = out_dir
+        .split(std::path::MAIN_SEPARATOR)
+        .nth_back(3)
+        .unwrap();
+
     match Command::new("common/scripts/report_build_info.sh").output() {
         Ok(output) => {
             for line in String::from_utf8(output.stdout).unwrap().lines() {
@@ -82,6 +90,10 @@ fn main() -> Result<(), anyhow::Error> {
     println!(
         "cargo:rustc-env=ZTUNNEL_BUILD_RUSTC_VERSION={}",
         rustc_version::version().unwrap()
+    );
+    println!(
+        "cargo:rustc-env=ZTUNNEL_BUILD_PROFILE_NAME={}",
+        profile_name
     );
     Ok(())
 }
