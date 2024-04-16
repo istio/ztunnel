2a2fa32edd8a8fc3e78849dafcf751b836eaf91b
Benjamin Leggett <benjamin.leggett@solo.io>
2024-04-16 11:33:59 -0400

    More comments
    Signed-off-by: Benjamin Leggett <benjamin.leggett@solo.io>

--- src/proxy/pool.rs
+++ src/proxy/pool.rs
@@ -66,6 +66,9 @@ pub struct WorkloadHBONEPool {
 }
 
 impl WorkloadHBONEPool {
+    // Creates a new pool instance, which should be owned by a single proxied workload.
+    // The pool will watch the provided drain signal and drain itself when notified.
+    // Callers should then be safe to drop() the pool instance.
     pub fn new(
         cfg: crate::config::Config,
         socket_factory: Arc<dyn SocketFactory + Send + Sync>,
@@ -92,9 +95,14 @@ impl WorkloadHBONEPool {
         }
     }
 
+    // Obtain a pooled connection. Will prefer to retrieve an existing conn from the pool, but
+    // if none exist, or the existing conn is maxed out on streamcount, will spawn a new one,
+    // even if it is to the same dest+port.
+    //
+    // If many `connects` request a connection  to the same dest at once, all will wait until exactly
+    // one connection is created, before deciding if they should create more or just use that one.
     pub async fn connect(&mut self, key: WorkloadKey) -> Result<Client, Error> {
-        // TODO BML this may not be collision resistant
-        // it may also be slow as shit
+        // TODO BML this may not be collision resistant/slow. It should be resistant enough for workloads tho.
         let mut s = DefaultHasher::new();
         key.hash(&mut s);
         let hash_key = s.finish();
@@ -103,6 +111,16 @@ impl WorkloadHBONEPool {
             GLOBAL_CONN_COUNT.fetch_add(1, Ordering::Relaxed),
         );
 
+        // First, see if we can naively just check out a connection.
+        // This should be the common case, except for the first establishment of a new connection/key.
+        // This will be done under outer readlock (nonexclusive)/inner keyed writelock (exclusive).
+        //
+        // It is important that the *initial* check here is authoritative, hence the locks, as
+        // we must know if this is a connection for a key *nobody* has tried to start yet,
+        // or if other things have already established conns for this key.
+        //
+        // This is so we can backpressure correctly if 1000 tasks all demand a new connection
+        // to the same key at once, and not eagerly open 1000 tunnel connections.
         let existing_conn = self
             .first_checkout_conn_from_pool(&key, hash_key, &pool_key)
             .await;
@@ -111,12 +129,22 @@ impl WorkloadHBONEPool {
             debug!("using existing conn, connect future will be dropped on the floor");
             Ok(existing_conn.unwrap())
         } else {
+            // We couldn't get a conn. This means either nobody has tried to establish any conns for this key yet,
+            // or they have, but no conns are currently available
+            // (because someone else has checked all of them out and not put any back yet)
+            //
+            // So, we wil writelock the outer lock to see if an inner lock entry exists for our key.
+            //
             // critical block - this writelocks the entire pool for all tasks/threads
-            // as we check to see if anyone has inserted a sharded mutex for this key.
-            // So we want to hold this for as short as possible a time, and drop it
+            // as we check to see if anyone has ever inserted a sharded mutex for this key.
+            //
+            // If not, we insert one.
+            //
+            // We want to hold this for as short as possible a time, and drop it
             // before we hold it over an await.
             //
-            // this is is the only block where we should hold a writelock on the whole mutex map
+            // this is is the ONLY block where we should hold a writelock on the whole mutex map
+            // for the rest, a readlock (nonexclusive) is sufficient.
             {
                 let mut map_write_lock = self.established_conn_writelock.write().await;
                 match map_write_lock.get(&hash_key) {
@@ -128,12 +156,15 @@ impl WorkloadHBONEPool {
                         map_write_lock.insert(hash_key, Some(Mutex::new(())));
                     }
                 };
-                drop(map_write_lock);
+                drop(map_write_lock); // strictly redundant
             }
 
-            // Now we know _someone_ won the race to insert a conn mutex, we don't need a writelock
-            // on the outer map anymore - so we can just readlock the outer map,
-            // and get the inner mutex for this connkey that we care about.
+
+
+            // If we get here, it means the following are true:
+            // 1. We have a guaranteed sharded mutex in the outer map for our current key.
+            // 2. We can now, under readlock(nonexclusive) in the outer map, attempt to
+            // take the inner writelock for our specific key (exclusive).
             //
             // This unblocks other tasks spawning connections against other keys, but blocks other
             // tasks spawning connections against THIS key - which is what we want.
@@ -149,13 +180,18 @@ impl WorkloadHBONEPool {
             // and we WANT other tasks to go back to sleep if there is an outstanding lock.
             // So the downsides are actually useful (we WANT task contention -
             // to block other parallel tasks from trying to spawn a connection if we are already doing so)
+            //
+            // BEGIN take outer readlock
             let map_read_lock = self.established_conn_writelock.read().await;
             let exist_conn_lock = map_read_lock.get(&hash_key).unwrap();
+            // BEGIN take inner writelock
             let found_conn = match exist_conn_lock.as_ref().unwrap().try_lock() {
                 Ok(_guard) => {
-                    // if we get here, either we won the connlock race and can create one,
-                    // or someone else won, but the streamcount for the one they added is already hit,
-                    // so we should start and insert another.
+                    // If we get here, it means the following are true:
+                    // 1. We did not get a connection for our key.
+                    // 2. We have the exclusive inner writelock to create a new connection for our key.
+                    //
+                    // So, carry on doing that.
                     debug!("appears we need a new conn, retaining connlock");
                     debug!("nothing else is creating a conn, make one");
                     let pool_conn = self.spawn_new_pool_conn(key.clone()).await;
@@ -171,14 +207,18 @@ impl WorkloadHBONEPool {
                     );
                     debug!("dropping lock");
                     Some(client)
+                    // END take inner writelock
                 }
                 Err(_) => {
-                    // The sharded mutex for this connkey is already locked - someone else must be making a conn
-                    // if they are, try to wait for it, but bail if we find one and it's got a maxed streamcount.
                     debug!("something else is creating a conn, wait for it");
-                    // let waiter = self.pool_watcher.changed();
-                    // tokio::pin!(waiter);
-
+                    // If we get here, it means the following are true:
+                    // 1. At one point, there was a preexisting conn in the pool for this key.
+                    // 2. When we checked, we got nothing for that key.
+                    // 3. We could not get the exclusive inner writelock to add a new one for this key.
+                    // 4. Someone else got the exclusive inner writelock, and is adding a new one for this key.
+                    //
+                    // So, loop and wait for the pool_watcher to tell us a new conn was enpooled,
+                    // so we can pull it out and check it.
                     loop {
                         match self.pool_watcher.changed().await {
                             Ok(_) => {
@@ -186,18 +226,17 @@ impl WorkloadHBONEPool {
                                     "notified a new conn was enpooled, checking for hash {:#?}",
                                     hash_key
                                 );
-
-                                let existing_conn = self.connected_pool.get(&hash_key); // .and_then(|e_conn| {
-
-                                //         debug!("while waiting for new conn, got existing conn for key {:#?}", key);
-                                // });
-                                match existing_conn {
+                                // The sharded mutex for this connkey is already locked - someone else must be making a conn
+                                // if they are, try to wait for it, but bail if we find one and it's got a maxed streamcount.
+                                let existing_conn = self.connected_pool.get(&hash_key);                               match existing_conn {
                                     None => {
-                                        debug!("got nothin");
+                                        debug!("got nothing");
                                         continue;
                                     }
                                     Some(e_conn) => {
                                         debug!("found existing conn after waiting");
+                                        // We found a conn, but it's already maxed out.
+                                        // Return None and create another.
                                         if e_conn.at_max_streamcount() {
                                             debug!("found existing conn for key {:#?}, but streamcount is maxed", key);
                                             break None;
@@ -207,6 +246,7 @@ impl WorkloadHBONEPool {
                                 }
                             }
                             Err(_) => {
+                                // END take outer readlock
                                 return Err(Error::WorkloadHBONEPoolDraining)
                             }
                         }
@@ -214,13 +254,35 @@ impl WorkloadHBONEPool {
                 }
             };
 
+
+            // If we get here, it means the following are true:
+            // 1. At one point, there was a preexisting conn in the pool for this key.
+            // 2. When we checked, we got nothing for that key.
+            // 3. We could not get the exclusive inner writelock to add a new one
+            // 4. Someone else got the exclusive inner writelock, and is adding a new one
+            // 5. We waited until we got that connection for our key, that someone else added.
+            //
+            // So now we are down to 2 options:
+            //
+            // 1. We got an existing connection, it's usable, we use it.
+            // 2. We got an existing connection, it's not usable, we start another.
+            //
+            // Note that for (2) we do not really need to take the inner writelock again for this key -
+            // if we checked out a conn inserted by someone else that we cannot use,
+            // everyone else will implicitly be waiting for us to check one back in,
+            // since they are in their own loops.
             match found_conn {
+                // After waiting, we found an available conn we can use, so no need to start another.
+                // Clone the underlying client, return a copy, and put the other back in the pool.
                 Some(f_conn) => {
                     self.connected_pool.put(&pool_key, f_conn.clone());
                     let _ = self.pool_notifier.send(true);
                     Ok(f_conn)
                 }
 
+                // After waiting, we found an available conn, but for whatever reason couldn't use it.
+                // (streamcount maxed, etc)
+                // Start a new one, clone the underlying client, return a copy, and put the other back in the pool.
                 None => {
                     debug!("spawning new conn for key {:#?} to replace", key);
                     let pool_conn = self.spawn_new_pool_conn(key.clone()).await;
@@ -234,6 +296,7 @@ impl WorkloadHBONEPool {
                     Ok(r_conn)
                 }
             }
+            // END take outer readlock
         }
     }
     async fn first_checkout_conn_from_pool(
