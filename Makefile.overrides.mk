343c909fe2053105924ef05233877f9fab61bd25
John Howard <john.howard@solo.io>
2024-04-25 11:46:19 -0700

    Support running tests in docker

--- Makefile.overrides.mk
+++ Makefile.overrides.mk
@@ -1,2 +1,7 @@
 # Use the build container by default
 BUILD_WITH_CONTAINER ?= 1
+# Namespaced tests need sys_admin due to docker being overly restrictive (https://github.com/moby/moby/issues/42441)
+# Ironically, this gives the container more privilege than is required without.
+DOCKER_RUN_OPTIONS += --privileged
+DOCKER_RUN_OPTIONS += -v /fake/path/does/not/exist:/var/run/netns
+DOCKER_RUN_OPTIONS += -v /dev/null:/run/xtables.lock
