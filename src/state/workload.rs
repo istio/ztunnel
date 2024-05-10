0f86994519cc3206e148d11c3d486288c3d7df08
John Howard <howardjohn@google.com>
2024-05-10 08:10:53 -0700

    adopt linting rules for rustc 1.78 (#1035)
    Well, turn one off.. I think its a bad rule and upstream may revert it

--- src/state/workload.rs
+++ src/state/workload.rs
@@ -669,7 +669,7 @@ impl WorkloadStore {
     }
 
     pub fn has_identity(&self, identity: &Identity) -> bool {
-        self.by_identity.get(identity).is_some()
+        self.by_identity.contains_key(identity)
     }
 }
 
