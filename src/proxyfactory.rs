2e415386382734b3f7be6882b460db39980e3bfe
John Howard <howardjohn@google.com>
2024-06-03 18:07:05 -0700

    dns: fix search lookup (#1099)
    * dns: fix search lookup
    
    Fixes https://github.com/istio/ztunnel/issues/1074 (issue was incorrect
    basically).
    
    Before our search domain was always ztunnel's (istio-system). This is incorrect; we
    should know the search domain of the request.
    
    We don't know the per-pod custom settings
    (https://github.com/istio/ztunnel/issues/555), but we can use the
    correct defaults.
    
    * Fix unwrap

--- src/proxyfactory.rs
+++ src/proxyfactory.rs
@@ -88,7 +88,10 @@ impl ProxyFactory {
                 self.config.dns_proxy_addr,
                 self.config.network.clone(),
                 self.state.clone(),
-                dns::forwarder_for_mode(self.config.proxy_mode)?,
+                dns::forwarder_for_mode(
+                    self.config.proxy_mode,
+                    self.config.cluster_domain.clone(),
+                )?,
                 self.dns_metrics.clone().unwrap(),
                 drain.clone(),
                 socket_factory.as_ref(),
