7561acbf0ae7abade56189478bc719c3b734a28a
Benjamin Leggett <benjamin.leggett@solo.io>
2024-06-14 13:06:32 -0400

    Fixup
    Signed-off-by: Benjamin Leggett <benjamin.leggett@solo.io>

--- src/inpod/workloadmanager.rs
+++ src/inpod/workloadmanager.rs
@@ -143,7 +143,7 @@ impl WorkloadProxyManager {
     }
 
     pub async fn run(mut self, drain: Watch) -> Result<(), anyhow::Error> {
-        self.run_internal(drain).await;
+        self.run_internal(drain).await?;
 
         // We broke the loop, this can only happen when drain was signaled
         // or we got a terminal protocol error. Drain our proxies.
@@ -158,7 +158,7 @@ impl WorkloadProxyManager {
     // - we have a ProtocolError (we have a serious version mismatch)
     // We should never _have_ a protocol error as the gRPC proto should be forwards+backwards compatible,
     // so this is mostly a safeguard
-    async fn run_internal(&mut self, drain: Watch) {
+    async fn run_internal(&mut self, drain: Watch) -> Result<(), anyhow::Error> {
         // for now just drop block_ready, until we support knowing that our state is in sync.
         debug!("workload proxy manager is running");
         // hold the  release shutdown until we are done with `state.drain` below.
@@ -186,7 +186,8 @@ impl WorkloadProxyManager {
                 }
                 Err(Error::ProtocolError) => {
                     error!("protocol mismatch error while processing stream, shutting down");
-                    return
+                    self.readiness.not_ready();
+                    return Err(anyhow::anyhow!("protocol error"))
                 }
                 Err(e) => {
                     // for other errors, just retry
@@ -196,6 +197,8 @@ impl WorkloadProxyManager {
             debug!("workload proxy manager is NOT ready");
             self.readiness.not_ready();
         };
+
+        Ok(())
     }
 }
 
