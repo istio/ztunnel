4cdd39775fe1f227ac8ecc5fb8fc11e3678b86cc
John Howard <john.howard@solo.io>
2024-08-23 08:09:50 -0700

    Cleanup redirection scripts
    We only use two now, clean up some stale ones and tidy up one we do use

--- scripts/ztunnel-redirect.sh
+++ scripts/ztunnel-redirect.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # shellcheck disable=SC2086
-# This script sets up redirection in the ztunnel network namespace for namespaced tests (tests/README.md)
+# This script sets up redirection in the ztunnel network namespace for namespaced tests for dedicated mode (tests/README.md)
+# See ztunnel-redirect-inpod.sh for inpod mode.
 set -ex
 
-# tproxy mark, it's only used here.
+# Mark ztunnel will set
 MARK=0x539/0xfff
-
-# Below is from config.sh but used in redirect-worker.sh as well
+# Port used for outbound traffic
 POD_OUTBOUND=15001
 
 set +e
@@ -40,24 +40,3 @@ $IPTABLES -w -t nat -F OUTPUT
 # We do not currently bother redirecting inbound traffic since we don't test it, but a more complete solution would.
 # Note: in real world, this would be a UID/GID match like sidecars. Setting mark is enabled only for testing (for now?)
 $IPTABLES -w -t nat -A OUTPUT -p tcp ! -o lo -m mark ! --mark $MARK -j REDIRECT --to-ports "${POD_OUTBOUND}"
-
-# With normal linux routing we need to disable the rp_filter
-# as we get packets from a tunnel that doesn't have default routes.
-echo 0 > /proc/sys/net/ipv4/conf/all/rp_filter
-echo 0 > /proc/sys/net/ipv4/conf/default/rp_filter
-echo 0 > /proc/sys/net/ipv4/conf/eth0/rp_filter
-#
-#$IPTABLES -t mangle -I PREROUTING -j LOG --log-prefix "mangle pre [zt] "
-#$IPTABLES -t mangle -I POSTROUTING -j LOG --log-prefix "mangle post [zt] "
-#$IPTABLES -t mangle -I INPUT -j LOG --log-prefix "mangle inp [zt] "
-#$IPTABLES -t mangle -I OUTPUT -j LOG --log-prefix "mangle out [zt] "
-#$IPTABLES -t mangle -I FORWARD -j LOG --log-prefix "mangle fw [zt] "
-#$IPTABLES -t nat -I POSTROUTING -j LOG --log-prefix "nat post [zt] "
-#$IPTABLES -t nat -I INPUT -j LOG --log-prefix "nat inp [zt] "
-#$IPTABLES -t nat -I OUTPUT -j LOG --log-prefix "nat out [zt] "
-#$IPTABLES -t nat -I PREROUTING -j LOG --log-prefix "nat pre [zt] "
-#$IPTABLES -t raw -I PREROUTING -j LOG --log-prefix "raw pre [zt] "
-#$IPTABLES -t raw -I OUTPUT -j LOG --log-prefix "raw out [zt] "
-#$IPTABLES -t filter -I FORWARD -j LOG --log-prefix "filt fw [zt] "
-#$IPTABLES -t filter -I OUTPUT -j LOG --log-prefix "filt out [zt] "
-#$IPTABLES -t filter -I INPUT -j LOG --log-prefix "filt inp [zt] "
