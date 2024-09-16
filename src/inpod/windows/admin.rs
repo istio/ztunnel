0d55da364540b55e6a48e6aa9ea58cad4af37a3a
Steven Jin Xuan <sjinxuan@microsoft.com>
2024-09-16 15:50:31 -0400

    NS capability checking

--- src/inpod/windows/admin.rs
+++ src/inpod/windows/admin.rs
@@ -3,5 +3,5 @@ use std::sync::RwLock;
 
 #[derive(Default)]
 pub struct WorkloadManagerAdminHandler {
-    state: RwLock<HashMap<crate::inpod::linux::WorkloadUid, ProxyState>>,
+    state: RwLock<HashMap<crate::inpod::windows::WorkloadUid, crate::state::ProxyState>>,
 }
