0d903947d0f3a4653abfabfea3696ea6fc747cf5
John Howard <john.howard@solo.io>
2024-11-22 12:29:33 -0800

    xds: gracefully handle stream termination without error
    Today, we only reconnect on the stream erroring. If it ends with a
    success, we ~infinite loop doing nothing.
    
    In standard operations, Istiod will never end the stream without an
    error. However, it is possible to expose Istiod with a proxy that does.

--- src/xds/client.rs
+++ src/xds/client.rs
@@ -440,7 +440,6 @@ pub struct Demander {
 
 #[derive(Debug)]
 enum XdsSignal {
-    None,
     Ack,
     Nack,
 }
@@ -448,7 +447,6 @@ enum XdsSignal {
 impl Display for XdsSignal {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         f.write_str(match self {
-            XdsSignal::None => "NONE",
             XdsSignal::Ack => "ACK",
             XdsSignal::Nack => "NACK",
         })
@@ -647,10 +645,14 @@ impl AdsClient {
                     self.handle_demand_event(_demand_event, &discovery_req_tx).await?;
                 }
                 msg = response_stream.message() => {
-                    let msg = msg?;
+                    let Some(msg) = msg? else {
+                        // If we got a None message, the stream ended without error.
+                        // This could be an explicit OK response, or if the stream is reset without a gRPC status.
+                        return Ok(());
+                    };
                     let mut received_type = None;
                     if !self.types_to_expect.is_empty() {
-                        received_type = msg.as_ref().map(|e| e.type_url.clone());
+                        received_type = Some(msg.type_url.clone())
                     }
                     if let XdsSignal::Ack = self.handle_stream_event(msg, &discovery_req_tx).await? {
                         if let Some(received_type) = received_type {
@@ -667,12 +669,9 @@ impl AdsClient {
 
     async fn handle_stream_event(
         &mut self,
-        stream_event: Option<DeltaDiscoveryResponse>,
+        response: DeltaDiscoveryResponse,
         send: &mpsc::Sender<DeltaDiscoveryRequest>,
     ) -> Result<XdsSignal, Error> {
-        let Some(response) = stream_event else {
-            return Ok(XdsSignal::None);
-        };
         let type_url = response.type_url.clone();
         let nonce = response.nonce.clone();
         self.metrics.record(&response, ());
