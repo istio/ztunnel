652e726add440e58af285923ae468eb1a697974d
Lior Lieberman <liorlieberman@google.com>
2024-12-12 22:14:31 +0000

    add PROXY_WORKLOAD_INFO env var to inpodserver

--- examples/inpodserver.rs
+++ examples/inpodserver.rs
@@ -13,13 +13,26 @@
 // limitations under the License.
 
 use std::os::fd::AsRawFd;
+
 use ztunnel::test_helpers::inpod::StartZtunnelMessage;
-use ztunnel::test_helpers::inpod::{start_ztunnel_server, Message};
+use ztunnel::{
+    inpod::istio::zds::WorkloadInfo,
+    test_helpers::inpod::{start_ztunnel_server, Message},
+};
+
+const PROXY_WORKLOAD_INFO: &str = "PROXY_WORKLOAD_INFO";
 
 #[cfg(target_os = "linux")]
 #[tokio::main]
 async fn main() {
     let uds = std::env::var("INPOD_UDS").unwrap();
+    let pwi = match parse_proxy_workload_info() {
+        Ok(pwi) => pwi,
+        Err(e) => {
+            eprintln!("Failed to parse proxy workload info: {:?}", e);
+            return;
+        }
+    };
     let netns = std::env::args().nth(1).unwrap();
     let mut netns_base_dir = std::path::PathBuf::from("/var/run/netns");
     netns_base_dir.push(netns);
@@ -31,12 +44,45 @@ async fn main() {
     sender
         .send(Message::Start(StartZtunnelMessage {
             uid: "uid-0".to_string(),
-            workload_info: None,
+            workload_info: Some(pwi),
             fd,
         }))
         .await
         .unwrap();
     sender.wait_forever().await.unwrap();
+}
+
+fn parse_proxy_workload_info() -> Result<WorkloadInfo, Error> {
+    let pwi = match std::env::var(PROXY_WORKLOAD_INFO) {
+        Ok(val) => val,
+        Err(_) => {
+            // Provide a default WorkloadInfo value if the environment variable is not set.
+            return Ok(WorkloadInfo {
+                name: "local".to_string(),
+                namespace: "default".to_string(),
+                service_account: "default".to_string(),
+            });
+        }
+    };
+
+    let s: Vec<&str> = pwi.splitn(3, "/").collect();
+    let &[ns, name, sa] = &s[..] else {
+        return Err(Error::InvalidArgument(format!(
+            "{PROXY_WORKLOAD_INFO} must match the format 'namespace/name/service-account' (got {s:?})"
+        )));
+    };
+
+    return Ok(WorkloadInfo {
+        name: name.to_string(),
+        namespace: ns.to_string(),
+        service_account: sa.to_string(),
+    });
+}
+
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+    #[error("invalid argument: {0}")]
+    InvalidArgument(String),
 }
 
 #[cfg(not(target_os = "linux"))]
