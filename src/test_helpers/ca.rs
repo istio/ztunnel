209eb50a0d9faa6a13cdad1c2406535b0b80063a
Zhewei Hu <zhewei.hu33@gmail.com>
2025-02-11 00:42:59 +0000

    Add unit tests

--- src/test_helpers/ca.rs
+++ src/test_helpers/ca.rs
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use std::collections::HashMap;
 use std::path::PathBuf;
 use std::time::Duration;
 
@@ -86,7 +87,7 @@ impl CaServer {
             ),
             true,
             60 * 60 * 24,
-            None,
+            HashMap::new(),
         )
         .await
         .unwrap();
