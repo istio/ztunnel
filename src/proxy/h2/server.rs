681ddf02ccb6d1ee9f6cf206598c940ab2aff84f
Jackie Maertens (Elliott) <64559656+jaellio@users.noreply.github.com>
2025-03-06 16:01:01 -0800

    Add inbound support for svc hostname in authority (#1420)
    * Support hostname in authority header
    
    Signed-off-by: Jackie Elliott <jaellio@microsoft.com>
    
    * remove todos
    
    Signed-off-by: Jackie Elliott <jaellio@microsoft.com>
    
    * Use enum for host addr
    
    Signed-off-by: Jackie Elliott <jaellio@microsoft.com>
    
    * Add HboneAddress enum and fix tests
    
    Signed-off-by: Jackie Elliott <jaellio@microsoft.com>
    
    * Update String usage to Strng and add comments
    
    Signed-off-by: Jackie Elliott <jaellio@microsoft.com>
    
    * Update svc_hostname to return strng
    
    Signed-off-by: Jackie Elliott <jaellio@microsoft.com>
    
    * Fix build errors
    
    Signed-off-by: Jackie Elliott <jaellio@microsoft.com>
    
    * Replace assert with panic for test configuration failure
    
    Signed-off-by: Jackie Elliott <jaellio@microsoft.com>
    
    * Fix fmt errors
    
    Signed-off-by: Jackie Elliott <jaellio@microsoft.com>
    
    * Respond to PR feedback
    
    Signed-off-by: Jackie Elliott <jaellio@microsoft.com>
    
    * Respond to some PR feedback
    
    Signed-off-by: Jackie Elliott <jaellio@microsoft.com>
    
    * Move HboneAddress to proxy
    
    Signed-off-by: Jackie Elliott <jaellio@microsoft.com>
    
    * Intelligently select service from hostname and vip from service.
    Implement TryFrom from uri to hboneAddress
    
    Signed-off-by: Jackie Elliott <jaellio@microsoft.com>
    
    * - Fix hbone_addr borrow and only pass port
    - Update hostname in proxy protocol to be hostname:port
    
    Signed-off-by: Jackie Elliott <jaellio@microsoft.com>
    
    * Update build_inbound_request to accept Parts to improve
    testability
    
    Signed-off-by: Jackie Elliott <jaellio@microsoft.com>
    
    * Add e2e for svc hostname
    
    Signed-off-by: Jackie Elliott <jaellio@microsoft.com>
    
    * clean up e2es
    
    Signed-off-by: Jackie Elliott <jaellio@microsoft.com>
    
    * Remove unused import
    
    Signed-off-by: Jackie Elliott <jaellio@microsoft.com>
    
    * Update service name to match hostname
    
    Signed-off-by: Jackie Elliott <jaellio@microsoft.com>
    
    * Rebase
    
    Signed-off-by: Jackie Elliott <jaellio@microsoft.com>
    
    * Add ipv4 check
    
    Signed-off-by: Jackie Elliott <jaellio@microsoft.com>
    
    Fix fmt
    
    Signed-off-by: Jackie Elliott <jaellio@microsoft.com>
    
    * Rebase and remove proxy protocol svc hostname
    
    Signed-off-by: Jackie Elliott <jaellio@microsoft.com>
    
    * remove failing test
    
    Signed-off-by: Jackie Elliott <jaellio@microsoft.com>
    
    * Translate service port to target port
    
    Signed-off-by: Jackie Elliott <jaellio@microsoft.com>
    
    * Fix unit tests
    
    Signed-off-by: Jackie Elliott <jaellio@microsoft.com>
    
    * fix nit
    
    Signed-off-by: Jackie Elliott <jaellio@microsoft.com>
    
    * Update error type
    
    Signed-off-by: Jackie Elliott <jaellio@microsoft.com>
    
    * Added support for named target ports
    
    Signed-off-by: Jackie Elliott <jaellio@microsoft.com>
    
    * Update namespaced tests to support multiple test cases
    
    Signed-off-by: Jackie Elliott <jaellio@microsoft.com>
    
    * Use arc service
    
    Signed-off-by: Jackie Elliott <jaellio@microsoft.com>
    
    * Reformat endpoint dereference
    
    Signed-off-by: Jackie Elliott <jaellio@microsoft.com>
    
    ---------
    
    Signed-off-by: Jackie Elliott <jaellio@microsoft.com>

--- src/proxy/h2/server.rs
+++ src/proxy/h2/server.rs
@@ -42,21 +42,6 @@ impl Debug for H2Request {
 }
 
 impl H2Request {
-    /// The request's method
-    pub fn method(&self) -> &http::Method {
-        &self.request.method
-    }
-
-    /// The request's URI
-    pub fn uri(&self) -> &http::Uri {
-        &self.request.uri
-    }
-
-    /// The request's headers
-    pub fn headers(&self) -> &http::HeaderMap<http::HeaderValue> {
-        &self.request.headers
-    }
-
     pub fn send_error(mut self, resp: Response<()>) -> Result<(), Error> {
         let _ = self.send.send_response(resp, true)?;
         Ok(())
@@ -78,6 +63,34 @@ impl H2Request {
         };
         let h2 = crate::proxy::h2::H2Stream { read, write };
         Ok(h2)
+    }
+
+    pub fn get_request(&self) -> &Parts {
+        &self.request
+    }
+
+    pub fn headers(&self) -> &http::HeaderMap<http::HeaderValue> {
+        self.request.headers()
+    }
+}
+
+pub trait RequestParts {
+    fn uri(&self) -> &http::Uri;
+    fn method(&self) -> &http::Method;
+    fn headers(&self) -> &http::HeaderMap<http::HeaderValue>;
+}
+
+impl RequestParts for Parts {
+    fn uri(&self) -> &http::Uri {
+        &self.uri
+    }
+
+    fn method(&self) -> &http::Method {
+        &self.method
+    }
+
+    fn headers(&self) -> &http::HeaderMap<http::HeaderValue> {
+        &self.headers
     }
 }
 
--- src/proxy/outbound.rs
+++ src/proxy/outbound.rs
@@ -27,7 +27,9 @@ use tracing::{Instrument, debug, error, info, info_span, trace_span};
 use crate::identity::Identity;
 
 use crate::proxy::metrics::Reporter;
-use crate::proxy::{BAGGAGE_HEADER, Error, ProxyInputs, TRACEPARENT_HEADER, TraceParent, util};
+use crate::proxy::{
+    BAGGAGE_HEADER, Error, HboneAddress, ProxyInputs, TRACEPARENT_HEADER, TraceParent, util,
+};
 use crate::proxy::{ConnectionOpen, ConnectionResult, DerivedWorkload, metrics};
 
 use crate::drain::DrainWatcher;
@@ -183,7 +185,7 @@ impl OutboundConnection {
         );
 
         let metrics = self.pi.metrics.clone();
-        let hbone_target = req.hbone_target_destination;
+        let hbone_target = req.hbone_target_destination.map(HboneAddress::SocketAddr);
         let result_tracker = Box::new(ConnectionResult::new(
             source_addr,
             req.actual_destination,
