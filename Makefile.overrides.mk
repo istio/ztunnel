dddd5ad653be426b426ff971c806e9b68806d471
zirain <zirain2009@gmail.com>
2025-04-08 22:52:15 +0800

    chore: fix make failed on mac (#1513)

--- Makefile.overrides.mk
+++ Makefile.overrides.mk
@@ -3,5 +3,7 @@ BUILD_WITH_CONTAINER ?= 1
 # Namespaced tests need sys_admin due to docker being overly restrictive (https://github.com/moby/moby/issues/42441)
 # Ironically, this gives the container more privilege than is required without.
 DOCKER_RUN_OPTIONS += --privileged
+ifeq ($(OS), Linux)
 DOCKER_RUN_OPTIONS += -v /fake/path/does/not/exist:/var/run/netns
+endif
 DOCKER_RUN_OPTIONS += -v /dev/null:/run/xtables.lock
