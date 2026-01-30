d3ea169748d381e381349c5f90da74c54ae45c5d
Zuzana Miklankova <zmiklank@redhat.com>
2026-01-30 10:03:10 +0100

    allow unusual byte groupings for openssl version detection
    Signed-off-by: Zuzana Miklankova <zmiklank@redhat.com>

--- build.rs
+++ build.rs
@@ -26,6 +26,7 @@ fn main() -> Result<(), anyhow::Error> {
     println!("cargo:rustc-check-cfg=cfg(ossl350)");
     if let Ok(v) = env::var("DEP_OPENSSL_VERSION_NUMBER") {
         let version = u64::from_str_radix(&v, 16).unwrap();
+            #[allow(clippy::unusual_byte_groupings)]
             if version >= 0x3_05_00_00_0 {
                 println!("cargo:rustc-cfg=ossl350");
             }
