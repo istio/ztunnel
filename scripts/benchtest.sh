4d6facf6024cb855e21f9449a52b9b023ec5a299
Matthew McLeod <mtthwmcld@gmail.com>
2026-07-28 21:01:24 +0100

    Benchmark end-to-end HBONE pool throughput

--- scripts/benchtest.sh
+++ scripts/benchtest.sh
@@ -12,5 +12,6 @@ cargo bench -- --baseline-lenient master
 # Capture profiles for the same-destination cases that expose pool contention.
 cargo bench --bench pool -- hbone_pool_same_destination_spawned/production/32 --profile-time 10
 cargo bench --bench pool -- hbone_pool_same_destination_spawned/production/512 --profile-time 10
+cargo bench --bench throughput -- hbone_pool_single_flow_throughput/pooled_hbone --profile-time 10
 
 cp -r "$REPORT_PATH" "$ARTIFACTS"
